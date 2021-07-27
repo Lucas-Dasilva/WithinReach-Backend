50ee2f784e9804d43e5d34f7751fbc9b18b26775
ldasilva <ldasilva@eecs.wsu.edu>
2021-07-27 13:17:18 -0700

    Fixed config secret key bug

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = app.secret_key = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     #SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:5829@localhost/WithinReachDb'
     #sqlite database is simply a file on our filesystem
